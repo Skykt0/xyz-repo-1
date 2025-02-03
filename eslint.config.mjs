bdfe2f32cf16625ebdfa4448935c03469d685c76
akashtanwar-318 <akash@cyntexa.com>
2025-02-03 14:46:44 +0530

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,37 @@
 export default [
-  // ESLint recommended configuration directly added to the array
-  {
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
+    // ESLint recommended configuration directly added to the array
+    {
+      ignores: ['public/js/require.js',
+        'public/js/postmonger.js'
+      ],
+      languageOptions: {
+        parserOptions: {
+          ecmaVersion: 2020,
+          sourceType: 'module',
+        },
+        globals: {
+          require: 'readonly',
+          module: 'readonly',
+          __dirname: 'readonly',
+          process: 'readonly',
+          structuredClone: 'readonly',
+          $: 'readonly',
+          define: 'readonly',
+          document: 'readonly',
+          window: 'readonly'
+        }
       },
-      globals: {
-        require: 'readonly',
-        module: 'readonly',
-        __dirname: 'readonly',
-        process: 'readonly',
-        structuredClone: 'readonly',
-        $: 'readonly',
-        define: 'readonly',
-        document: 'readonly',
-        window: 'readonly'
-      }
+      rules: {
+        'no-console': 'error',
+        'eqeqeq': 'error',
+        'no-unused-vars': 'warn',
+        'no-debugger': 'error',
+        'no-undef': 'error',
+        'no-unreachable': 'error',
+        'curly': 'error',
+        'semi': ['error', 'always'],
+        'indent': ['error', 2],
+        'quotes': ['error', 'single'],
+      },
     },
-    rules: {
-      'no-console': 'error',
-      'eqeqeq': 'error',
-      'no-unused-vars': 'warn',
-      'no-debugger': 'error',
-      'no-undef': 'error',
-      'no-unreachable': 'error',
-      'curly': 'error',
-      'semi': ['error', 'always'],
-      'indent': ['error', 2],
-      'quotes': ['error', 'single'],
-    },
-  },
-];
+  ];
