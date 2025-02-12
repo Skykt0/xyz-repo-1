7d31ec90887ac303cdc64c843896d02aa2353c0c
akashtanwar-318 <akash@cyntexa.com>
2025-02-12 14:25:28 +0530

    ultility Helper changes

--- public/js/utilities/utilityHelper.js
+++ public/js/utilities/utilityHelper.js
@@ -1,26 +1,30 @@
-function base64ToFile(base64String, fileName) {
-  let arr = base64String.split(',');
-  let mime = arr[0].match(/:(.*?);/)[1];
-  let byteString = atob(arr[1]);
-
-  let arrayBuffer = new Uint8Array(byteString.length);
-  for (let i = 0; i < byteString.length; i++) {
-    arrayBuffer[i] = byteString.charCodeAt(i);
+define([], function () {
+  function base64ToFile(base64String, fileName) {
+    let arr = base64String.split(',');
+    let mime = arr[0].match(/:(.*?);/)[1];
+    let byteString = atob(arr[1]);
+  
+    let arrayBuffer = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) {
+      arrayBuffer[i] = byteString.charCodeAt(i);
+    }
+  
+    let file = new File([arrayBuffer], fileName, { type: mime });
+  
+    return file;
+  };
+  
+  function convertToBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result.split(',')[1]);
+      reader.onerror = (error) => reject(error);
+      reader.readAsDataURL(file);
+    });
   }
 
-  let file = new File([arrayBuffer], fileName, { type: mime });
-
-  return file;
-};
-
-
-function convertToBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = () => resolve(reader.result.split(',')[1]);
-    reader.onerror = (error) => reject(error);
-    reader.readAsDataURL(file);
-  });
-}
-
-export { base64ToFile, convertToBase64};
+  return {
+    base64ToFile: base64ToFile,
+    convertToBase64: convertToBase64
+  };
+});
