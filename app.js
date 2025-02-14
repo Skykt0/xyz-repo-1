a30b06111ac7c94608bed39a3e6e0a1c75a35e36
akashtanwar-318 <akash@cyntexa.com>
2025-02-14 14:34:07 +0530

    payloadtoolarge error fix

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const app = express();
 
 // Configure Express
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.json({ type: 'application/json' }));
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
