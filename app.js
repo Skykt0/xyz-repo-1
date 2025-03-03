a5edf19b51e90127b40a753c92cd33776d29f832
akashtanwar-318 <akash@cyntexa.com>
2025-03-03 12:49:44 +0530

    code refactoring

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 
 const path = require('path');
 var activity = require('./routes/activity');
 const app = express();
 
-// Configure Express
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
