a8bbe9f1d767bbd55329cc2d91cbe81200f7bb62
akashtanwar-318 <akash@cyntexa.com>
2025-04-14 17:25:33 +0530

    css change

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -300,8 +300,10 @@ define([
             $('.html-editor-front').addClass('show');
             if(selectedCreationType === 'pdf-creation-type' || selectedCreationType === 'template-creation-type') {
               $(`.${selectedMessageType} .${selectedCreationType}`).removeClass('html');
+              $(`.${selectedMessageType} .${selectedCreationType}`).addClass('template');
               if(selectedCardInsertDesignFormat === 'html') {
                 $(`.${selectedMessageType} .${selectedCreationType}`).addClass('html');
+                $(`.${selectedMessageType} .${selectedCreationType}`).removeClass('template');
               }
             }
           }
@@ -896,40 +898,12 @@ define([
     if ($(`.${selectedMessageType} .screen-2`).css('display') === 'block') {
       const pdfLinkElement = $(`.${selectedMessageType} .screen-2 .pdfLink`);
       let isDescriptionValid = validateInputField($(`.${selectedMessageType} .screen-2 .description`));
+      let frontTemplateValid = validateInputField($(`.${selectedMessageType} .screen-2 .frontTemplate`));
       pdfLinkElement.siblings('.error-msg').text('Please enter required field');
       let isPdfLinkValid = validateInputField(pdfLinkElement);
       
-      if (!isDescriptionValid || !isPdfLinkValid) {
+      if (!isDescriptionValid || !isPdfLinkValid || !frontTemplateValid) {
         isValid = false;
-      }
-
-      if(selectedMessageType === 'LettersCardInsert') {
-        if(selectedCardInsertDesignFormat === 'html') {
-          let cardfrontHtmlContent = $(`.${selectedMessageType} .screen-2 .html-editor-front-card-insert`).val().trim();
-          let cardfrontHtmlBtnLabel = $(`.${selectedMessageType} .screen-2 .html-editor-front-card-insert`).data('btn-label');
-          let cardBackHtmlElement = $(`.${selectedMessageType} .screen-2 .html-editor-back-card-insert`);
-          let cardBackHtmlContent = cardBackHtmlElement.val() === undefined || cardBackHtmlElement.hasClass('hidden') ? undefined : cardBackHtmlElement.val().trim();
-          let cardBackHtmlBtnLabel = cardBackHtmlElement.val() === undefined || cardBackHtmlElement.hasClass('hidden') ? undefined : cardBackHtmlElement.data('btn-label');
-          let htmlEditorErrorMsg = $(`.${selectedMessageType} .screen-2 .html-editor .error-msg`);
-
-          if (cardfrontHtmlContent === '' || cardBackHtmlContent === '') {
-            isValid = false;
-            let missingFields = [];
-            if (cardfrontHtmlContent === '') {missingFields.push(cardfrontHtmlBtnLabel);}
-            if (cardBackHtmlContent === '') {missingFields.push(cardBackHtmlBtnLabel);}
-
-            if (missingFields.length > 0) {
-              htmlEditorErrorMsg.text(`Please enter content in the following fields: ${missingFields.join(', ')}.`).addClass('show');
-            }
-          } else { 
-            htmlEditorErrorMsg.removeClass('show');
-          }
-        } else {
-          let isValidInput = validateInputField($(`.${selectedMessageType} .screen-2 .card-insert-input-${selectedCardInsertDesignFormat} input`));
-          if(!isValidInput) {
-            isValid = false;
-          }
-        }
       }
   
       if (selectedMessageType === 'trifold') {
